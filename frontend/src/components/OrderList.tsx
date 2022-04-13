7493f2f11505f9c630dd85eaa0b3d4a3fefa9741
Eric Marcantonio <eric11marcantonio@gmail.com>
2022-04-13 17:36:48 -0400

    fixed the order time bug

--- frontend/src/components/OrderList.tsx
+++ frontend/src/components/OrderList.tsx
@@ -85,7 +85,7 @@ export const OrderComponent = (props: IOrderComponentProps) => {
           <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">Date</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {moment.utc(props.order.order.updatedAt).local().format("MMMM Do YYYY")}
+              {moment(props.order.order.updatedAt).utc().local().format("MMMM Do YYYY")}
             </dd>
           </div>
           <div className="bg-white px-4 py-5">
