b99a367699d84fdae881ce909baf441e0842e809
MichelleMali <mm0015@my.yorku.ca>
2022-04-12 23:26:31 -0400

    Rollback Payment Page pages cause of rendering issue, will circle back

--- frontend/src/PaymentPage.tsx
+++ frontend/src/PaymentPage.tsx
@@ -1,22 +1,13 @@
-import { useNavigate } from "react-router";
 import Navbar from "./components/Navbar";
 import PaymentForm from "./components/PaymentForm";
-import { container } from "./GlobalContainer";
 
 const PaymentPage = () => {
-  const navigate = useNavigate();
-  const con = container.useContainer();
-  
     return (
       <>
-      if (con.address.firstname) {
-        <><Navbar /><PaymentForm /></>
-      }
-      else  {
-        navigate(`/address`)
-      }
+        <Navbar />
+        <PaymentForm />
       </>
     );
-};
+  };
 
 export default PaymentPage;
