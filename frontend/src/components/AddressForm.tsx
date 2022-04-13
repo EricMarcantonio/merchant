56bb4f61734d82cb03f6074e824cd15308e149e9
MichelleMali <mm0015@my.yorku.ca>
2022-04-13 18:38:04 -0400

    Form validation done for log in, sign up, address, and payment forms

--- frontend/src/components/AddressForm.tsx
+++ frontend/src/components/AddressForm.tsx
@@ -36,7 +36,23 @@ const AddressForm = () => {
               Enter your name and address:
             </p>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form
+            className="mt-8 space-y-6"
+            action="#"
+            method="POST"
+            onSubmit={() => {
+              con.setAddress({
+                firstname: firstname,
+                lastname: lastname,
+                street: street,
+                province: province,
+                zip: zip,
+                phone: phoneNumber,
+                country: country,
+              });
+              navigate(`/payment`);
+            }}
+          >
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
@@ -171,18 +187,6 @@ const AddressForm = () => {
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                onClick={() => {
-                  con.setAddress({
-                    firstname: firstname,
-                    lastname: lastname,
-                    street: street,
-                    province: province,
-                    zip: zip,
-                    phone: phoneNumber,
-                    country: country,
-                  });
-                  navigate(`/payment`);
-                }}
               >
                 Proceed To Payment
               </button>
--- frontend/src/components/LogInForm.tsx
+++ frontend/src/components/LogInForm.tsx
@@ -53,10 +53,15 @@ const LogInForm = () => {
                 </a>
               </p>
             </div>
-            <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={(e) => {
-                    e.preventDefault();
-                    handleAdminLogin(email, password);
-                  }}>
+            <form
+              className="mt-8 space-y-6"
+              action="#"
+              method="POST"
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleAdminLogin(email, password);
+              }}
+            >
               <input type="hidden" name="remember" defaultValue="true" />
               <div className="rounded-md shadow-sm -space-y-px">
                 <div>
@@ -101,7 +106,6 @@ const LogInForm = () => {
                 <button
                   type="submit"
                   className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  
                 >
                   <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                     <LockClosedIcon
