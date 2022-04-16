022a6e8c02e611d704ded0c883a90bf47bce71b7
Eric Marcantonio <eric11marcantonio@gmail.com>
2022-04-16 14:59:40 -0400

    good

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -1,11 +1,56 @@
 #!/usr/bin/env bash
 ID=$(aws sts get-caller-identity --query "Account" | tr -d \")
-REGION='ca-central-1'
-AWS_SECRET_ACCESS_KEY='DSbuAAh9lfYmglZku/5LSglQgCbX7QMuwjcwdAaN'
-AWS_ACCESS_KEY_ID='AKIARRVFVRZBVAWSTMDY'
-aws ecr get-login-password --region 'ca-central-1' | docker login --username AWS --password-stdin "${ID}.dkr.ecr.${REGION}.amazonaws.com"
 
+REGION='us-east-1'
 
-AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} docker --context myecscontext compose -f docker-compose-aws.yml  up
+aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin "${ID}.dkr.ecr.${REGION}.amazonaws.com"
 
+aws ecr create-repository --repository-name merchant/auth --region ${REGION}
+aws ecr create-repository --repository-name merchant/catalog --region ${REGION}
+aws ecr create-repository --repository-name merchant/orders --region ${REGION}
+aws ecr create-repository --repository-name merchant/shopping-cart --region ${REGION}
+aws ecr create-repository --repository-name merchant/reviews --region ${REGION}
+aws ecr create-repository --repository-name merchant/visiting --region ${REGION}
+aws ecr create-repository --repository-name merchant/payment --region ${REGION}
+aws ecr create-repository --repository-name merchant/nginx --region ${REGION}
 
+cd ../catalog && make docker-build
+cd ../orders && make docker-build
+cd ../payment && make docker-build
+cd ../reviews && make docker-build
+cd ../shopping-cart && make docker-build
+cd ../visiting && make docker-build
+cd ../nginx && make docker-build
+cd ../auth && make docker-build
+
+docker tag merchant/catalog "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/catalog:latest"
+docker push "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/catalog:latest"
+
+docker tag merchant/orders "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/orders:latest"
+docker push "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/orders:latest"
+
+docker tag merchant/payment "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/payment:latest"
+docker push "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/payment:latest"
+
+docker tag merchant/reviews "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/reviews:latest"
+docker push "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/reviews:latest"
+
+docker tag merchant/shopping-cart "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/shopping-cart:latest"
+docker push "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/shopping-cart:latest"
+
+docker tag merchant/visiting "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/visiting:latest"
+docker push "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/visiting:latest"
+
+docker tag merchant/nginx "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/nginx:latest"
+docker push "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/nginx:latest"
+
+docker tag merchant/auth "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/auth:latest"
+docker push "${ID}.dkr.ecr.${REGION}.amazonaws.com/merchant/auth:latest"
+
+aws cloudformation deploy \
+  --region "${REGION}" \
+  --template-file possible_good.yml \
+  --stack-name merchant \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides AccountId="${ID}" \
+  --parameter-overrides Region="${REGION}"
