41b2cb44e3c22140b8a3517373aa09652a7b6732
Eric Marcantonio <eric11marcantonio@gmail.com>
2022-04-16 20:10:08 -0400

    MVP

--- aws/create-repos.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-aws ecr create-repository --repository-name merchant/auth --region us-east-1
-aws ecr create-repository --repository-name merchant/catalog --region us-east-1
-aws ecr create-repository --repository-name merchant/orders --region us-east-1
-aws ecr create-repository --repository-name merchant/shopping-cart --region us-east-1
-aws ecr create-repository --repository-name merchant/reviews --region us-east-1
-aws ecr create-repository --repository-name merchant/visiting --region us-east-1
-aws ecr create-repository --repository-name merchant/payment --region us-east-1
-aws ecr create-repository --repository-name merchant/nginx --region us-east-1
